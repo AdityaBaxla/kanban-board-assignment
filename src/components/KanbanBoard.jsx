ad20c87cc5258466c82901c3911c15a788cb7e62
AdityaBaxla <adityabaxla@gmail.com>
2023-11-01 21:39:51 +0530

    multiple kanban card from json

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import KanbanCard from './KanbanCard';
 import PriorityBar from "./PriorityBar"
 
 const KanbanBoard = () => {
-  const dummyCardData = {
+  /*const dummyCardData = {
     id: 'CAM-1',
     title: 'Conduct Security Vulnerability Assesment',
     tag: ['Feature Request'],
@@ -11,15 +11,54 @@ const KanbanBoard = () => {
     priority: 2,
     status: 'In Progress',
   };
-  
+  const dummyCardData2 = {
+    id: 'CAM-2',
+    title: 'Off Security Vulnerability Assesment',
+    tag: ['Feature Request'],
+    userId: 'usr-2',
+    priority: 1,
+    status: 'In Progress',
+  };
+  */
+
+  const [toDo, setToDo] = useState([
+  {
+    "id": "CAM-1",
+    "title": "Update User Profile Page UI",
+    "tag": ["Feature request"],
+    "userId": "usr-1",
+    "status": "Todo",
+    "priority": 4
+  },
+  {
+    "id": "CAM-2",
+    "title": "Add Multi-Language Support - Enable multi-language support within the application.",
+    "tag": ["Feature Request"],
+    "userId": "usr-2",
+    "status": "In progress",
+    "priority": 3
+  },
+  {
+    "id": "CAM-3",
+    "title": "Optimize Database Queries for Performance",
+    "tag": ["Feature Request"],
+    "userId": "usr-2",
+    "status": "In progress",
+    "priority": 1
+  },
+  // ... other items
+]);
+
 
   return (
     <div className="kanban-board">
       <div className="column">
-        <PriorityBar/>
-        <KanbanCard {...dummyCardData} />
+        {toDo && toDo.map(task => {
+          return(
+            <KanbanCard{...task}/>
+          )
+        })}
       </div>
-      {/* ... Other columns and cards */}
     </div>
   );
 };
